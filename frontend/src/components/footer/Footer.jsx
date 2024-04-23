22ccefcd1281b29d8d14379242018e45d04d951e
mohammadnoor <mohammadnoorcse@gmail.com>
2024-04-23 12:39:21 +0600

    working main footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -28,6 +28,68 @@ export default function Footer() {
             </div>
         </div>
     </div>
+     
+     <div className="main-footer content-center w-100">
+        <div className="main-footer-content content">
+            <div>
+                <div className="item-1">
+                    <span>Ecom-Bazar</span>
+                    <p>Got Question? Call us 9 AM- 10 PM</p>
+                    <h1>01775296704</h1>
+                </div>
+                <div className="item-2">
+                    <p>Follow Us</p>
+                    <div>
+                        <Link to="/"><i class="fa-brands fa-facebook"></i></Link>
+                        <Link to="/"><i class="fa-brands fa-twitter"></i></Link>
+                        <Link to="/"><i class="fa-brands fa-linkedin"></i></Link>
+                        <Link to="/"><i class="fa-brands fa-youtube"></i></Link>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <span>COMPANY</span>
+                <div>
+                    <Link to="/">About Us</Link>
+                    <Link to="/">Career</Link>
+                    <Link to="/">Contact Us</Link>
+                    <Link to="/">Privacy Policy</Link>
+                    <Link to="/">Ecom-bazar Certified</Link>
+                    <Link to="/">Terms & Condition</Link>
+                    <Link to="/">Next/Same day delivery TC</Link>
+                </div>
+            </div>
+            <div>
+                <span>MY ACCOUNT</span>
+                <div>
+                    <Link to="/">Sign In</Link>
+                    <Link to="/">Orders</Link>
+                    <Link to="/">Addresses</Link>
+                    <Link to="/">My Wishlist</Link>
+                    <Link to="/">Order History</Link>
+                    <Link to="/">Track My Order</Link>
+                    
+                </div>
+            </div>
+            <div>
+                <span>CUSTOMER SERVICE</span>
+                <div>
+                    <Link to="/">Payment Methods</Link>
+                    <Link to="/">Support Center</Link>
+                    <Link to="/">How To Shop On Ecom-bazar</Link>
+                    <Link to="/">Featured Recommendation</Link>
+                    <Link to="/">Cancellation, Return & Refund</Link>
+                
+                </div>
+            </div>
+
+        </div>
+     </div>
+     <div className="bottom-footer centent-center w-100">
+        <div className="bottom-footer-content content">
+
+        </div>
+     </div>
     </>
   )
 }
