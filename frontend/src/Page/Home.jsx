864e590ec06c95eb433914cbf66cf60461165037
mohammadnoor <mohammadnoorcse@gmail.com>
2024-04-24 16:13:14 +0600

    working third section

--- frontend/src/Page/Home.jsx
+++ frontend/src/Page/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import TopSection from '../components/home/TopSection'
+import SecondSection from '../components/home/SecondSection'
+import ThirdSection from '../components/home/ThirdSection'
 
 export default function Home() {
   return (
     <>
      <TopSection/>
+     <SecondSection/>
+     <ThirdSection/>
     
     </>
   )
