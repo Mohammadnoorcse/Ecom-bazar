1891353f816b78d5f4ec26a9469aae4460de7b03
mohammadnoor <mohammadnoorcse@gmail.com>
2024-04-26 20:24:45 +0600

    working filter of product page

--- frontend/src/components/product/AllProduct.jsx
+++ frontend/src/components/product/AllProduct.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./allproduct.css";
 import {product} from "../home/home.js"
 import ProductCard from '../home/ProductCard.jsx';
 
 export default function AllProduct() {
+  const [filterItem,setFilterItem] = useState(false);
+  console.log(filterItem)
+  
   return (
     <div className='allproduct content-center w-100'>
         <div className="allproduct-content content">
@@ -13,9 +16,40 @@ export default function AllProduct() {
                 ))}
             </div>
 
-            {/* <div className="allproduct-filter">
-
-            </div> */}
+            <div className="allproduct-filter">
+              <i class="fa-solid fa-list" onClick={()=>filterItem?setFilterItem(false):setFilterItem(true)}></i>
+               <div className={filterItem?"filter-item":"filter-item-display"}>
+                <div className="filter-item-1">
+                    <div className='filter-item-title'>
+                      <span>Catgeory</span>
+                    </div>
+                    <div className='filter-item-value'>
+                      <input type="checkbox" value="box" />
+                      <span>box</span>
+                    </div>
+                    <div className='filter-item-value'>
+                      <input type="checkbox" value="box" />
+                      <span>box</span>
+                    </div>
+                    <div className='filter-item-value'>
+                      <input type="checkbox" value="box" />
+                      <span>box</span>
+                    </div>
+                    <div className='filter-item-value'>
+                      <input type="checkbox" value="box" />
+                      <span>box</span>
+                    </div>
+                    <div className='filter-item-value'>
+                      <input type="checkbox" value="box" />
+                      <span>box</span>
+                    </div>
+                    <div className='filter-item-value'>
+                      <input type="checkbox" value="box" />
+                      <span>box</span>
+                    </div>
+                </div>
+               </div>
+            </div>
 
         </div>
 
