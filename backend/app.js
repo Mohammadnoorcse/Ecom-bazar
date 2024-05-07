5b6e414dbecf380553840e0bb9c5574abf88ac8f
mohammadnoor <mohammadnoorcse@gmail.com>
2024-05-07 10:43:18 +0600

    working the backend

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,9 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '500mb'}));
+app.use(express.urlencoded({limit: '500mb'}));
+// app.use(express.limit(100000000));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
